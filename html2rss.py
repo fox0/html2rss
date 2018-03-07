86122d7d8427a8b577b9e2b565ce7ffe8304d210
fox <fox@mail.altstu.ru>
2018-03-07 15:02:52 +0700

    beautifulsoup4 and lxml

--- html2rss.py
+++ html2rss.py
@@ -1,62 +1,74 @@
 #!/usr/bin/env python3
+import re
 import sys
 from os.path import dirname, join
-from xml.etree import cElementTree as et
 from json import JSONDecoder
 import requests
-from htmldom import htmldom
+from bs4 import BeautifulSoup
+from lxml import etree as ET
+from lxml.builder import E
 
 
-def main(url):
-    rules = load_rules()
-    site = rules[url]  # TODO re
-    try:
-        headers = {'User-Agent': site['user_agent']}
-    except KeyError:
-        headers = None
-
-    html = requests.get(url, headers=headers).text
-    dom = htmldom.HtmlDom().createDom(html)
-    context = {
-        'link': url,
-        'title': dom.find('title').text(),
-    }
-
-    item = site['item']
-    if isinstance(item, str):
-        items = [{'description': i.text().replace('\n', '<br>')} for i in dom.find(item)]
-    else:
-        raise NotImplementedError
-
-    print('<?xml version="1.0" encoding="utf-8"?>')
-    et.dump(generate_xml(context, items))
-    sys.exit(0)
+def main(url='http://medstories.net/'):
+    print(Parser(url).to_rss())
 
 
 def load_rules():
-    """
-    item - обязательный параметр. Строка или словарь.
-    user_agent - опционально
-    """
     with open(join(dirname(__file__), 'rules.json')) as f:
-        return JSONDecoder().decode(f.read())
+        d = JSONDecoder().decode(f.read())
+    return list((re.compile(expr), rule) for expr, rule in d.items())
 
 
-def generate_xml(context, items):
-    rss = et.Element('rss')
-    channel = et.SubElement(rss, 'channel')
-    _add_subelement(channel, context)
-    for i in items:
-        item = et.SubElement(channel, 'item')
-        _add_subelement(item, i)
-    return et.ElementTree(rss)
+class Parser(object):
+    rules = load_rules()
 
+    def __init__(self, url):
+        self.url = url
+        self.rule = self._get_rule()
+        self.soup = BeautifulSoup(self._get_html(), 'lxml')
 
-def _add_subelement(parent, d):
-    for k, v in d.items():
-        et.SubElement(parent, k).text = v
+    def _get_rule(self):
+        for expr, rule in self.rules:
+            if expr.match(self.url):
+                return rule
+        raise Exception('Not found rule for url "%s"' % self.url)
+
+    def _get_html(self):
+        try:
+            headers = {'User-Agent': self.rule['user_agent']}
+        except KeyError:
+            headers = None
+        response = requests.get(self.url, headers=headers)
+        response.raise_for_status()
+        return response.text
+
+    def to_rss(self):
+        rss = E.rss(
+            E.channel(
+                E.title(self.soup.title.string),
+                E.link(self.url),
+                E.description(self.soup.find('meta', attrs={'name': 'description'})['content']),
+                *self._get_items()), version='2.0')
+        return ET.tostring(rss, xml_declaration=True, encoding='utf-8').decode('utf-8')
+
+    def _get_items(self):
+        result = []
+        for i in self.soup.select(self.rule['item']):
+            result.append(E.item(
+                E.description(i.__unicode__())
+            ))
+        # for i in self.soup.select(self.rule['parent']):
+        #     ls = []
+        #     for k in ('title',):
+        #         try:
+        #             selector = self.rule[k]
+        #             a = i.select(selector)
+        #             ls.append(a)
+        #         except KeyError:
+        #             pass
+        #     result.append(E.item(*ls))
+        return result
 
 
 if __name__ == '__main__':
-    # main('http://medstories.net/')
-    main(sys.argv[1])
+    main(*sys.argv[1:])
