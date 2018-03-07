f7537277cbb58c812e8997343ad858b5c668b1ac
fox <fox@mail.altstu.ru>
2018-03-07 23:57:06 +0700

    +paginator

--- html2rss.py
+++ html2rss.py
@@ -22,7 +22,7 @@ class Parser(object):
     def __init__(self, url):
         self.url = url
         self.rule = self._get_rule()
-        self.soup = BeautifulSoup(self._get_html(), 'lxml')
+        self.parsed_uri = urlparse(self.url)
 
     def _get_rule(self):
         for expr, rule in self.rules:
@@ -30,39 +30,50 @@ class Parser(object):
                 return rule
         raise Exception('Not found rule for url "%s"' % self.url)
 
-    def _get_html(self):
+    def to_rss(self):
+        ls = []
+        soup = self._get_soup(self.url)
+        ls.extend(self._get_feed_info(soup))
+        ls.extend(self._get_items(soup))  # first page
+
+        paginator = self.rule.get('paginator')
+        if paginator:
+            for i in range(2, paginator['count'] + 1):
+                url = '{url}?{name}={count}'.format(url=self.url, name=paginator['name'], count=i)  # todo
+                soup = self._get_soup(url)
+                ls.extend(self._get_items(soup))
+
+        rss = E.rss(E.channel(*ls), version='2.0')
+        return ET.tostring(rss, xml_declaration=True, encoding='utf-8').decode('utf-8')
+
+    def _get_soup(self, url):
         try:
             headers = {'User-Agent': self.rule['user_agent']}
         except KeyError:
             headers = None
-        response = requests.get(self.url, headers=headers)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
-        return response.text
+        return BeautifulSoup(response.text, 'lxml')
 
-    def to_rss(self):
-        ls = self._get_items()
-        ls.append(E.title(self.soup.title.string))
-        ls.append(E.link(self.url))
-        try:
-            t = self.soup.find('meta', attrs={'name': 'description'})['content']
-            ls.append(E.description(t))
-        except TypeError:
-            pass
-        rss = E.rss(E.channel(*ls), version='2.0')
-        return ET.tostring(rss, xml_declaration=True, encoding='utf-8').decode('utf-8')
+    def _get_feed_info(self, soup):
+        result = [E.title(soup.find('title').text), E.link(self.url)]
+        t = soup.find('meta', attrs={'name': 'description'})
+        if t:
+            result.append(E.description(t['content']))
+        return result
 
-    def _get_items(self):
+    def _get_items(self, soup):
         result = []
-        parsed_uri = urlparse(self.url)
-        p = self.rule['item'].pop('parent')
-        for tag in self.soup.find_all(p['tag'], attrs=p.get('attrs', {})):
+        rule = self.rule['item'].copy()
+        sel = rule.pop('parent')
+        for tag in soup.find_all(sel['tag'], attrs=sel.get('attrs', {})):
             ls = []
-            for k, p in self.rule['item'].items():
-                tag2 = tag.find(p['tag'], attrs=p.get('attrs', {}))
+            for k, sel in rule.items():
+                tag2 = tag.find(sel['tag'], attrs=sel.get('attrs', {}))
                 # todo rewrite?
                 if k == 'link':
                     href = tag2.find('a')['href']
-                    text = '{uri.scheme}://{uri.netloc}{href}'.format(uri=parsed_uri, href=href)
+                    text = '{uri.scheme}://{uri.netloc}{href}'.format(uri=self.parsed_uri, href=href)
                     ls.append(E.guid(text))  # todo?
                 elif k == 'description':
                     text = tag2.__unicode__()
