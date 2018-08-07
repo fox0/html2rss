2f5a33ab92953acd30cf047f4e3e73afff318007
fox0 <red-fox0@yandex.ru>
2018-08-07 19:54:37 +0700

    +slice

--- html2rss.py
+++ html2rss.py
@@ -16,46 +16,55 @@ log = logging.getLogger(__name__)
 
 def load_rules():
     filename = os.path.join(os.path.dirname(__file__), 'rules.json')
-    with open(filename) as f:  # todo cssselect?
+    with open(filename) as f:
         d = JSONDecoder().decode(f.read())
     return list((re.compile(expr), rule) for expr, rule in d.items())
 
 
+rules = load_rules()
+
+
+def _get_rule(url):
+    for expr, rule in rules:
+        if expr.match(url):
+            return rule
+    raise Exception('Not found rule for url "%s"' % url)
+
+
 class Parser(object):
-    rules = load_rules()
 
     def __init__(self, url):
         self.url = url
-        self.rule = self._get_rule()
-        self.parsed_uri = urlparse(self.url)
-
-    def _get_rule(self):
-        for expr, rule in self.rules:
-            if expr.match(self.url):
-                return rule
-        raise Exception('Not found rule for url "%s"' % self.url)
+        self.rule = _get_rule(url)
 
     def to_rss(self):
         ls = []
-        soup = self._get_soup(self.url)  # (!)
-        ls.extend(self._get_feed_info(soup))
-        ls.extend(self._get_items(soup))  # first page
 
-        paginator = self.rule.get('paginator')
-        if paginator:
-            for i in range(2, paginator['count'] + 1):
-                url = '{url}?{name}={count}'.format(url=self.url, name=paginator['name'], count=i)  # todo
+        if 'paginator' in self.rule:
+            for i in range(1, self.rule['paginator']['count'] + 1):
+                url = '{url}?{name}={count}'.format(url=self.url, name=self.rule['paginator']['name'], count=i)
                 soup = self._get_soup(url)
                 ls.extend(self._get_items(soup))
+        else:
+            soup = self._get_soup(self.url)
+            ls.extend(self._get_items(soup))
+        ls.extend(self._get_feed_info(soup))
 
         rss = E.rss(E.channel(*ls), version='2.0')
         return ET.tostring(rss, xml_declaration=True, encoding='utf-8').decode('utf-8')
 
     def _get_soup(self, url):
-        try:
-            headers = {'User-Agent': self.rule['user_agent']}
-        except KeyError:
-            headers = None
+        headers = None
+        if self.rule.get('user_agent', False):
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language': 'ru',
+                'DNT': '1',
+                'Upgrade-Insecure-Requests': '1',
+                'Cache-Control': 'max-age=0',
+            }
+            log.debug(headers)
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         # return BeautifulSoup(response.text, 'lxml')  # глючит с https://pikabu.ru/@iLawyer
@@ -70,33 +79,26 @@ class Parser(object):
 
     def _get_items(self, soup):
         result = []
-        rule = self.rule['item'].copy()
-        sel = rule.pop('parent')
-        queryset = soup.find_all(sel['tag'], attrs=sel.get('attrs', {}))
-        if not queryset:
-            log.error('Not found parent tag "%s"', sel)
+        queryset = self._find_all(soup, 'parent')
+        if 'count' in self.rule:
+            queryset = queryset[:self.rule['count']]
         for tag in queryset:
             ls = []
-            for k, sel in rule.items():
-                tag2 = tag.find(sel['tag'], attrs=sel.get('attrs', {}))
-                if not tag2:
-                    log.warning('Not found tag "%s". Ignore it.', sel)
-                    continue
-                if k == 'link':
-                    href = tag2.find('a')['href']
-                    if not href:
-                        log.warning('Not found link')
-                    uri2 = urlparse(href)
-                    log.debug(uri2)
-                    text = '{uri.scheme}://{uri.netloc}{uri2.path}'.format(uri=self.parsed_uri, uri2=uri2)
-                    ls.append(E.guid(text))
-                    log.debug(text)
-                elif k == 'description':
-                    text = tag2.__unicode__()
-                else:
-                    text = tag2.text.strip()
-                ls.append(E.__getattr__(k)(text))
+            href = self._find_all(tag, 'link')[0].find('a')['href']
+            text = '{uri.scheme}://{uri.netloc}{uri2.path}'.format(uri=urlparse(self.url), uri2=urlparse(href))
+            log.debug(text)
+            ls.append(E.link(text))
+            ls.append(E.guid(text))
+            ls.append(E.title(self._find_all(tag, 'title' if 'title' in self.rule else 'link')[0].text.strip()))
+            ls.append(E.description(self._find_all(tag, 'text')[0].__unicode__()))
             result.append(E.item(*ls))
+        return result
+
+    def _find_all(self, soup, sel):
+        rule = self.rule[sel]
+        result = soup.find_all(rule['tag'], attrs=rule.get('attrs', {}))
+        if not result:
+            log.warning('Not found tag "%s"', rule)
         return result
 
 
@@ -105,4 +107,4 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         # sys.argv.append('http://medstories.net/')
         sys.argv.append('https://pikabu.ru/@iLawyer')
-    print(Parser(sys.argv[1]).to_rss())
+    sys.stdout.write(Parser(sys.argv[1]).to_rss())
