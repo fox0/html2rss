59bdba181868e4ef29a17e73a85b2841a6af6d56
fox0 <red-fox0@yandex.ru>
2019-06-21 23:48:25 +0700

    -xml

--- html2rss.py
+++ html2rss.py
@@ -5,11 +5,10 @@ import logging
 import email.utils
 from datetime import datetime
 from urllib.parse import urlparse
-from lxml import etree as ET
-from lxml.builder import E
 
 import requests
 from bs4 import BeautifulSoup
+from markupsafe import Markup
 
 log = logging.getLogger(__name__)
 
@@ -20,18 +19,26 @@ def http_parser(url, rules):
     soup = BeautifulSoup(r.text, 'lxml')
     for i in soup.find_all(rules['tag']):
         url2 = i.find_all('a', attrs=_get_attrs(rules.get('link.attrs')))[0]['href']
-        link = '{uri.scheme}://{uri.netloc}{uri2.path}'.format(uri=urlparse(url), uri2=urlparse(url2))
-        yield E.item(
-            E.link(link),
-            E.guid(link),
-            E.title(i.find_all(rules.get('title.tag'), attrs=_get_attrs(rules.get('title.tag.attrs')))[0].text.strip()),
-            E.description(i.find_all(rules.get('text.tag'), attrs=_get_attrs(rules.get('text.attrs')))[0].__unicode__())
-        )
+        yield Markup('''\
+<link>{uri.scheme}://{uri.netloc}{uri2.path}</link>
+<guid>{uri.scheme}://{uri.netloc}{uri2.path}</guid>
+<title>{title}</title>
+<description>{text}</description>''').format(
+            uri=urlparse(url),
+            uri2=urlparse(url2),
+            title=find_tag(i, rules, 'title').text.strip(),
+            text=find_tag(i, rules, 'text').__unicode__())
+
+
+def find_tag(soup, rules, name):
+    tag = rules.get(name + '.tag')
+    attrs = rules.get(name + '.attrs')
+    return soup.find_all(tag, attrs=_get_attrs(attrs))[0]
 
 
 def _get_attrs(rule):
     """
-    >>>_get_attrs('class=visit-link')
+    >>> _get_attrs('class=visit-link')
     {'class': 'visit-link'}
     """
     if rule is None:
@@ -50,21 +57,22 @@ def vk_api(url, rules):
     api = API()
     r = api.wall_get(domain=domain)
     for i in r['items']:
-        link = 'https://vk.com/wall%d_%d' % (i['owner_id'], i['id'])
-        yield E.item(
-            E.link(link),
-            E.guid(link),
-            E.title('#%d' % i['id']),
-            E.description(i['text'].replace('\n', '<br>')),
-            E.pubDate(email.utils.format_datetime(datetime.fromtimestamp(i['date']))),  # todo tz
-        )
+        yield Markup('''\
+<link>https://vk.com/wall{i[owner_id]}_{i[id]}</link>
+<guid>https://vk.com/wall{i[owner_id]}_{i[id]}</guid>
+<title>#{i[id]}</title>
+<description>{text}</description>
+<pubDate>{date}</pubDate>''').format(
+            i=i,
+            text=i['text'].replace('\n', '<br>'),
+            date=email.utils.format_datetime(datetime.fromtimestamp(i['date'])))  # todo tz
 
 
 def main(*urls):
     config = configparser.ConfigParser()
     config.read('config.ini')
 
-    result = [E.link(urls[0])]
+    result = []
     for url in urls:
         host = urlparse(url).netloc
         log.debug('host=%s', host)
@@ -75,8 +83,15 @@ def main(*urls):
         func = globals()[rules['source']]
         for i in func(url, rules):
             result.append(i)
-    rss = E.rss(E.channel(*result), version='2.0')
-    return ET.tostring(rss, xml_declaration=True, encoding='utf-8').decode('utf-8')
+
+    return '''\
+<?xml version='1.0' encoding='utf-8'?>
+<rss version="2.0">
+    <channel>
+        <link>{link}</link>
+        <item>{items}</item>
+    </channel>
+</rss>'''.format(link=urls[0], items='</item>\n<item>'.join(result))
 
 
 if __name__ == '__main__':
