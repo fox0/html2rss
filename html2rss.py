f71204e45b5c16e685a317074fcdf544ec8646f5
fox0 <red-fox0@yandex.ru>
2019-06-23 21:11:16 +0700

    cssselect

--- html2rss.py
+++ html2rss.py
@@ -18,15 +18,17 @@ def http_parser(url, rules):
     r = requests.get(url)
     r.raise_for_status()
     soup = BeautifulSoup(r.text, 'lxml')
-    for i in soup.find_all(rules['tag']):
-        url2 = i.find_all('a', attrs=_get_attrs(rules.get('link.attrs')))[0]['href']
-        title = find_tag(i, rules, 'title').text.strip()
+    for i in soup.select(rules['selector']):
+        url2 = i.select(rules['link'])[0]['href']
+        title = i.select(rules['title'])[0].text.strip()
+
         f = rules.getint('title.filter')
         if f:
             ls = re.findall(r'(\d+)', title)
             log.debug('%s | %s', ls, title)
             if ls and int(ls[0]) <= f:
                 continue
+
         yield Markup('''\
 <link>{uri.scheme}://{uri.netloc}{uri2.path}</link>
 <guid>{uri.scheme}://{uri.netloc}{uri2.path}</guid>
@@ -35,27 +37,7 @@ def http_parser(url, rules):
             uri=urlparse(url),
             uri2=urlparse(url2),
             title=title,
-            text=find_tag(i, rules, 'text').__unicode__())
-
-
-def find_tag(soup, rules, name):
-    tag = rules.get(name + '.tag')
-    attrs = rules.get(name + '.attrs')
-    return soup.find_all(tag, attrs=_get_attrs(attrs))[0]
-
-
-def _get_attrs(rule):
-    """
-    >>> _get_attrs('class=visit-link')
-    {'class': 'visit-link'}
-    """
-    if rule is None:
-        return {}
-    r = {}
-    for i in rule.split(' '):
-        k, v = i.split('=')
-        r[k] = v
-    return r
+            text=i.select(rules['text'])[0])
 
 
 def vk_api(url, rules):
@@ -76,6 +58,12 @@ def vk_api(url, rules):
             date=email.utils.format_datetime(datetime.fromtimestamp(i['date'])))  # todo tz
 
 
+sources = {
+    'vk_api': vk_api,
+    'http_parser': http_parser,
+}
+
+
 def main(*urls):
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -88,10 +76,7 @@ def main(*urls):
             log.error('invalid host %s', host)
             continue
         rules = config[host]
-        func = {
-            'vk_api': vk_api,
-            'http_parser': http_parser,
-        }[rules['source']]
+        func = sources[rules['source']]
         for i in func(url, rules):
             result.append(i)
 
